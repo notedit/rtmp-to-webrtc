65b8cc09d3c04b14690001d08638fe3a2410d468
notedit <notedit@gmail.com>
2018-04-02 00:46:31 +0800

    add code

---
+++ mediaserver.js
@@ -0,0 +1,19 @@
+const medoozeMediaServer = require('medooze-media-server');
+//Get Semantic SDP objects
+const SemanticSDP	= require('semantic-sdp');
+const SDPInfo		= SemanticSDP.SDPInfo;
+const MediaInfo		= SemanticSDP.MediaInfo;
+const CandidateInfo	= SemanticSDP.CandidateInfo;
+const DTLSInfo		= SemanticSDP.DTLSInfo;
+const ICEInfo		= SemanticSDP.ICEInfo;
+const StreamInfo	= SemanticSDP.StreamInfo;
+const TrackInfo		= SemanticSDP.TrackInfo;
+const Direction		= SemanticSDP.Direction;
+const CodecInfo		= SemanticSDP.CodecInfo;
+
+
+
+
+
+
+
---
+++ server.js
@@ -0,0 +1,29 @@
+const NodeMediaServer = require('node-media-server');
+
+const config = {
+    rtmp: {
+        port: 1935,
+        chunk_size: 1024,
+        gop_cache: true,
+        ping: 60,
+        ping_timeout: 30
+    },
+    http: {
+        port: 8000,
+        allow_origin: '*'
+    }
+};
+
+var nms = new NodeMediaServer(config)
+nms.run();
+
+
+nms.on('postPublish', (id, StreamPath, args) => {
+    console.log('[NodeEvent on postPublish]', `id=${id} StreamPath=${StreamPath} args=${JSON.stringify(args)}`);
+    // we need to transcode
+    
+});
+
+nms.on('donePublish', (id, StreamPath, args) => {
+    console.log('[NodeEvent on donePublish]', `id=${id} StreamPath=${StreamPath} args=${JSON.stringify(args)}`);
+});
