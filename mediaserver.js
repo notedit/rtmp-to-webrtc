ac237679f5c89e229e5b9e9e399e22a0a615b46c
notedit <notedit@gmail.com>
2018-04-02 01:25:03 +0800

    add

--- mediaserver.js
+++ mediaserver.js
@@ -13,7 +13,36 @@ const CodecInfo		= SemanticSDP.CodecInfo;
 
 
 
+class MediaServer 
+{
+    constructor(publicIp)
+    {
+        this.endpoint = medoozeMediaServer.createEndpoint(publicIp);
+        medoozeMediaServer.enableDebug(true);
+        medoozeMediaServer.enableUltraDebug(true);
 
+        this.streams = {};
+    }
+
+    async createStream(streamName)
+    {
+        const streamer = medoozeMediaServer.createStreamer();
+        const video = new MediaInfo("video","video");
+        
+        //Add h264 codec
+        video.addCodec(new CodecInfo("h264",96));
+        //Create session for video
+
+        //https://github.com/medooze/media-server-demo-node/blob/master/index.js#L522
+        const session = streamer.createSession(video, {
+	        local  : {
+                port: 5004
+	        }
+        });
+
+
+    }
+}
 
 
 
